b91a1c4bfc21882a9adb8accae6e3f0da5802896
Tomas Dominguez <tdominguez2014@outlook.com>
2017-01-05 13:32:07 -0500

    practice

--- ConsoleApplication1/Strings/Program.cs
+++ ConsoleApplication1/Strings/Program.cs
@@ -10,18 +10,30 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            string emptyString = String.Empty;
-            string nullString = null;
+            //string emptyString = String.Empty;
+            //string nullString = null;
 
-            Console.WriteLine(emptyString); // prints nothing
-            Console.WriteLine(nullString); // prints nothing
+            //Console.WriteLine(emptyString); // prints nothing
+            //Console.WriteLine(nullString); // prints nothing
 
-            // this line will print 0
-            Console.WriteLine($"1st string is {emptyString.Length} characters long.");
-            //by adding the $ it shows the numeric value between the {}
+            //// this line will print 0
+            //Console.WriteLine($"1st string is {emptyString.Length} characters long.");
+            ////by adding the $ it shows the numeric value between the {}
 
-            // this line will throw an exception (uncomment it to confirm)
-            // Console.WriteLine($"2nd string is {nullString.Length} characters long.");
+            //// this line will throw an exception (uncomment it to confirm)
+            //// Console.WriteLine($"2nd string is {nullString.Length} characters long.");
+
+            string name = "Tom";
+            string greeting1 = $"Hello {name} !";
+            string greeting2 = $"Hello {name.ToUpper()} !";
+            string greeting3 = $"Hello {name.ToLower()} !";
+            string greeting4 = "Hello " + name + " !";
+
+            Console.WriteLine(name);
+            Console.WriteLine(greeting1);
+            Console.WriteLine(greeting2);
+            Console.WriteLine(greeting3);
+            Console.WriteLine(greeting4);
 
             Console.ReadLine();
         }
