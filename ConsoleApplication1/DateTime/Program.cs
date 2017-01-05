41027c625a2f1c43e81989212c2b1fe587fb05eb
Tomas Dominguez <tdominguez2014@outlook.com>
2017-01-05 14:51:46 -0500

    change names to prevent issues

--- ConsoleApplication1/DateTime/Program.cs
+++ ConsoleApplication1/DateTime/Program.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DateTime
+namespace DateTimePractice
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            DateTime nextYear = new DateTime(DateTime.Today.Year + 1, 1, 1);
+            TimeSpan duration = nextYear - DateTime.Today;
+            Console.WriteLine($"There are {duration.TotalDays} days left in the year");
         }
     }
 }
