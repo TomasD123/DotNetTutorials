332dda48ecc1a531343726686cf49aede25c837b
Tomas Dominguez <tdominguez2014@outlook.com>
2017-01-04 22:58:51 -0500

    second run

--- ConsoleApplication1/ConsoleApplication2/Program.cs
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            var name = "Jack"; 
+            Console.WriteLine($"Hello {name}!");
         }
     }
 }
