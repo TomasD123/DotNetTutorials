f80afc4b91fa5509c95c0ab61616a7533e713e35
Tomas Dominguez <tdominguez2014@outlook.com>
2017-01-08 18:40:19 -0500

    update

--- ConsoleApplication1/DateTime/Program.cs
+++ ConsoleApplication1/DateTime/Program.cs
@@ -75,6 +75,7 @@ namespace DateTimePractice
                     Console.WriteLine($"Your 10,000 day Anniversay falls on {anniversary}");
                 }
             }
+
             Console.ReadLine();
         }
     }
