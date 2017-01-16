edb9a03eeb810c46453d465fe4ccb0d622e69c96
Tom <Tom@DESKTOP-MQ9F0U9>
2017-01-16 08:12:57 -0500

    updated making decision

--- ConsoleApplication1/MakingDecisionIfElseSwitch/Program.cs
+++ ConsoleApplication1/MakingDecisionIfElseSwitch/Program.cs
@@ -11,34 +11,57 @@ namespace MakingDecisionIfElseSwitch
         static void Main(string[] args)
         {
             //simple switch example
-                //uint guessedNumber = 1;
-                //switch (guessedNumber)
-                //{
-                //    case 0:
-                //        Console.WriteLine("Sorry, 0 is not a valid guess.");
-                //        break;
-                //    case 1:
-                //    case 2:
-                //    case 3:
-                //        Console.WriteLine("You guessed low.");
-                //        break;
-                //    case 4:
-                //        Console.WriteLine("You guessed the right number!");
-                //        break;
-                //    default:
-                //        Console.WriteLine("You guessed high.");
-                //        break;
-                //}
+            //uint guessedNumber = 1;
+            //switch (guessedNumber)
+            //{
+            //    case 0:
+            //        Console.WriteLine("Sorry, 0 is not a valid guess.");
+            //        break;
+            //    case 1:
+            //    case 2:
+            //    case 3:
+            //        Console.WriteLine("You guessed low.");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("You guessed the right number!");
+            //        break;
+            //    default:
+            //        Console.WriteLine("You guessed high.");
+            //        break;
+            //}
 
-            int correctNumber = new Random().Next(4);
+            int guessedNumber;
 
-            Console.WriteLine($"{correctNumber}");
+            int randomNumber = new Random().Next(4);
 
-            int guessedNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{randomNumber}");
 
-            //get user input
+            Console.WriteLine($"Please guess a number between 1 and 3");
+            
+            do
+            {
+                guessedNumber = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"this is your guess {guessedNumber}");  
+                if (guessedNumber < 1 || guessedNumber > 3)
+                {
+                    Console.WriteLine("You have guessed a number outside the range between one and three, please try again");
+                }
+
+            }while (guessedNumber < 1 || guessedNumber > 3);
+
+            
+            if (guessedNumber == randomNumber)
+            {
+                Console.WriteLine("You have guessed right");
+            }  
+            if (guessedNumber > randomNumber)
+            {
+                Console.WriteLine("Your guessed number was to high");
+            }  
+            if (guessedNumber < randomNumber)
+            {
+                Console.WriteLine("Your guessed number was to low");
+            } 
 
             Console.ReadLine();
         }
