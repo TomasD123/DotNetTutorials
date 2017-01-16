f492e5d0477feab689c47aba0cfdf1c76350f5ce
Tomas Dominguez <dominguez.tom@icloud.com>
2017-01-16 17:53:39 -0500

    updated logical operations to include time for greating

--- ConsoleApplication1/LogicalOperators/Program.cs
+++ ConsoleApplication1/LogicalOperators/Program.cs
@@ -14,26 +14,41 @@ namespace LogicalOperators
 
             var name = Console.ReadLine();
 
+            //get curernt time to compare
             DateTime currentTime = DateTime.Now;
             int year = currentTime.Year;
             int month = currentTime.Month;
             int day = currentTime.Day;
 
+            //set new value for when it is considered morning, evening, night (afternoon is between morning evening)
             var morningTime = new DateTime(year, month, day, 11, 59, 59);
-            var afternoonTime = new DateTime(year, month, day, 12, 0, 0);
-            var eveningTime = new DateTime(year, month, day, 17, 59, 59);
-            
-            var morning = (currentTime < morningTime );
+            var eveningTime = new DateTime(year, month, day, 16, 59, 59);
+            var nightTime = new DateTime(year, month, day, 20, 0, 0);
+
+            //create logical and check curernt time to expected greeting
+            var morning = (currentTime < morningTime);
             var afternoon = (currentTime > morningTime && currentTime < eveningTime);
+            var evening = (currentTime > eveningTime && currentTime < nightTime);
+            var night = (currentTime > nightTime);
 
             if (morning == true)
             {
-                Console.WriteLine("Good Monring");
+                Console.WriteLine("Good Monring ");
             }
             if (afternoon == true)
             {
                 Console.WriteLine("Good Afternoon");
             }
+            if (evening == true)
+            {
+                Console.WriteLine("Good Evening");
+            }
+            if (night == true)
+            {
+                Console.WriteLine("Good Night");
+            }
+
+            Console.ReadLine();
         }
     }
 }
