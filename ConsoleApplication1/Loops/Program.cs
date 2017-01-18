1e2ae46bad68071708324927f46121674281d5ea
Tomas Dominguez <dominguez.tom@icloud.com>
2017-01-17 23:21:05 -0500

    started Loops

---
+++ ConsoleApplication1/Loops/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i <= 24; i += 4)
+            //{
+            //    Console.Write($" {i} ");
+
+            //    for (int j = 0; j <= 24; j += 3)
+            //    {
+            //        Console.Write($" {j} ");
+
+                    for (int k = 0; k <= 2; k += 2)
+                    {
+                        Console.Write($" {k} ");
+
+                        for (int l = 0; l <= 24; l += 1)
+                        {
+                            Console.Write($" {l} ");
+                        }
+
+                        Console.WriteLine();
+                    }
+
+            //    }
+            //}
+            Console.ReadLine();
+        }
+    }
+}
